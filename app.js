efbfc4ea773fac04d6787ccc922d4ea40be042fa
programmermohaimin <you@example.com>
2022-08-24 08:31:40 +0600

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+let bun = document.getElementById('bun');
+let navLink = document.getElementById('navLink');
+
+bun.addEventListener('click', function () {
+    navLink.classList.toggle('active')
+})
